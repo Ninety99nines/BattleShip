6d0610e2d387df2835c23d83ca882d9a9afa8d35
surpriseddoggo <55451397+surpriseddoggo@users.noreply.github.com>
2021-12-29 08:24:45 -0500

    Update main.py

--- battleShipPY/main.py
+++ battleShipPY/main.py
@@ -3,7 +3,7 @@ from random import randint
 
 board = []
 
-for x in range(5):
+for x in range(0, 5):
   board.append(["O"] * 5)
 
 def print_board(board):
@@ -23,24 +23,26 @@ ship_col = random_col(board)
 print (ship_row)
 print (ship_col)
 
-# Everything from here on should go in your for loop!
-maxTurns = 10
-# Be sure to indent four spaces!
-for turn in range(maxTurns):
-    print ("Turn: " ,turn)
-    guess_row = int(input("Guess Row: "))
-    guess_col = int(input("Guess Col: "))
+# Everything from here on should be in your for loop
+# don't forget to properly indent!
+for turn in range(4):
+  print ("Turn", turn + 1)
+  guess_row = int(input("Guess Row: "))
+  guess_col = int(input("Guess Col: "))
 
-    if guess_row == ship_row and guess_col == ship_col:
-        print ("Congratulations! You sunk my battleship!")
-        break;
+  if guess_row == ship_row and guess_col == ship_col:
+    print ("Congratulations! You sank my battleship!")
+    break;   
+  else:
+    if guess_row not in range(5) or \
+      guess_col not in range(5):
+      print ("Oops, that's not even in the ocean.")
+    elif board[guess_row][guess_col] == "X":
+      print( "You guessed that one already." )
     else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print ("Oops, that's not even in the ocean.")
-        elif(board[guess_row][guess_col] == "X"):
-            print ("You guessed that one already.")
-        else:
-            print ("You missed my battleship!")
-            board[guess_row][guess_col] = "X"
-    print_board(board)
+      print ("You missed my battleship!")
+      board[guess_row][guess_col] = "X"
+  if turn == 3:
+      print("Game Over")
+print_board(board)
 
